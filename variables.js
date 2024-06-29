f997c0aa2e3302a7459a1453c2771e314f22603c
Afnan Hassan 2.0 <afnanirshad4070@gmail.com>
2024-06-29 05:30:11 +0500

    initial commit

---
+++ variables.js
@@ -0,0 +1,8 @@
+const accountID = 1133;
+var accountName = "Afnan Hassan";
+let accountOwner = "UBL";
+accountCity = "Karachi";
+
+
+console.log(accountID);
+console.table([accountID, accountName, accountOwner, accountCity]);
