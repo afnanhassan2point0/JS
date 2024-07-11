162418aa974d8779a3724b81419e66133080483a
Afnan Hassan 2.0 <afnanirshad4070@gmail.com>
2024-07-11 16:03:06 +0500

    fixed buggy input

--- HowToGetUserInputs.js
+++ HowToGetUserInputs.js
@@ -1,11 +1,18 @@
 
 
-import readline from 'readline'; // this module has standard input/output library/interface
-const userInput = readline.createInterface({ // gets input & output operations
-    valueIn: process.stdin,
-    valueOut: process.stdout
+const readline = require('readline'); // this module has standard input/output library/interface
+const rl = readline.createInterface({ // gets input & output operations
+    input: process.stdin,
+    output: process.stdout
 });
 
-userInput.question('Please enter Your name : ', (valueIn) => {
-    const name = valueIn;
+var name = ""; // variables initialization
+var age = 18;
+
+// getting user inputs
+rl.question('Please enter Your name : ', (name) => {
+    rl.question('Now, enter your age : ', (age) => {
+        console.log(`Hello ${name}, Nice to meet you at ${age}!`);
+        rl.close(); // necessary to close this at the end
+    });
 });
